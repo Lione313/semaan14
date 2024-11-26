15e0523d22f7c760096ac3af17a49ea618a87ee1
vlionel1 <dilanaimituma@gmail.com>
2024-11-26 13:56:59 -0500

    cmabios backen

---
+++ src/main/java/com/tecsup/practica/semana14/Semana14Application.java
@@ -0,0 +1,13 @@
+package com.tecsup.practica.semana14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Semana14Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Semana14Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/tecsup/practica/semana14/Semana14ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecsup.practica.semana14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semana14ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
